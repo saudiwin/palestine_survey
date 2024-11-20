b17f2fe9e0ef71d30c76fa93ce02f834cdd3b16d
Bob Kubinec <bobkubinec@gmail.com>
2024-11-20 14:25:02 -0500

    figure 1

--- palestine_daggity.R
+++ palestine_daggity.R
@@ -1,14 +1,33 @@
 # directed acyclic graph for Palestine study
 
-require(dagitty)
+# Load required libraries
+library(ggdag)
+library(ggplot2)
+library(patchwork) # For combining plots
 
+# Define DAGs for each panel
+dag1 <- dagify(
+  A ~ W,
+  W ~ D,
+  labels = c(D = "D", W = "W", A = "A")
+)
 
-pal_graph1 <- dagitty("dag{
-                      D -> C
-                      C -> A
-}")
+dag2 <- dagify(
+  W ~ D,
+  D ~ A,
+  labels = c(A = "A", D = "D", W = "W")
+)
 
-pal_graph2 <- dagitty("dag{
-                      C -> D
-                      D -> A
-}")
+dag3 <- dagify(
+  D ~ W,
+  W ~ A,
+  labels = c(A = "A", D = "D", W = "W")
+)
+
+# Create DAG plots
+plot1 <- ggdag(dag1) + theme_dag() + ggtitle("(1)")
+plot2 <- ggdag(dag2) + theme_dag() + ggtitle("(2)")
+plot3 <- ggdag(dag3) + theme_dag() + ggtitle("(3)")
+
+# Combine the plots side by side
+plot1 + plot2 + plot3
